25dc19b566cc881040482cfafda4c40692eff849
Amila Sikalo <asikalo2@etf.unsa.ba>
2019-01-31 13:18:57 +0100

    Dodavanje fxml tabova.

--- src/ba/unsa/etf/rpr/projekat/Controller.java
+++ src/ba/unsa/etf/rpr/projekat/Controller.java
@@ -1,4 +1,83 @@
 package ba.unsa.etf.rpr.projekat;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void postaviSQLite(ActionEvent actionEvent) {
+    }
+
+    public void postaviXML(ActionEvent actionEvent) {
+    }
+
+    public void addAirline(ActionEvent actionEvent) {
+    }
+
+    public void removeAirline(ActionEvent actionEvent) {
+    }
+
+    public void editAirline(ActionEvent actionEvent) {
+    }
+
+    public void addAirplane(ActionEvent actionEvent) {
+    }
+
+    public void removeAirplane(ActionEvent actionEvent) {
+    }
+
+    public void editAirplane(ActionEvent actionEvent) {
+    }
+
+    public void addFlight(ActionEvent actionEvent) {
+    }
+
+    public void removeFlight(ActionEvent actionEvent) {
+    }
+
+    public void editFlight(ActionEvent actionEvent) {
+    }
+
+    public void addFlightType(ActionEvent actionEvent) {
+    }
+
+    public void removeFlightType(ActionEvent actionEvent) {
+    }
+
+    public void editFlightType(ActionEvent actionEvent) {
+    }
+
+    public void addLuggage(ActionEvent actionEvent) {
+    }
+
+    public void removeLuggage(ActionEvent actionEvent) {
+    }
+
+    public void editLuggage(ActionEvent actionEvent) {
+    }
+
+    public void addPassenger(ActionEvent actionEvent) {
+    }
+
+    public void removePassenger(ActionEvent actionEvent) {
+    }
+
+    public void editPassenger(ActionEvent actionEvent) {
+    }
+
+    public void addUser(ActionEvent actionEvent) {
+    }
+
+    public void removeUser(ActionEvent actionEvent) {
+    }
+
+    public void editUser(ActionEvent actionEvent) {
+    }
+
+    public void addRole(ActionEvent actionEvent) {
+    }
+
+    public void removeRole(ActionEvent actionEvent) {
+    }
+
+    public void editRole(ActionEvent actionEvent) {
+    }
 }
