c0c25b0b5dfa72c1d56249b02a87c69a4726ca0a
asikalo2 <asikalo2@etf.unsa.ba>
2020-01-18 14:57:04 +0100

    Forgot to add report files and classes.

---
+++ src/ba/unsa/etf/rpr/projekat/PassengerListReport.java
@@ -0,0 +1,35 @@
+package ba.unsa.etf.rpr.projekat;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.swing.JRViewer;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class PassengerListReport extends JFrame {
+    public void showReport( List<Passenger> passengerList, Flight flight) throws JRException {
+        String reportSrcFile = getClass().getResource("/reports/PassengerReportList.jrxml").getFile();
+        String reportsDir = getClass().getResource("/reports/").getFile();
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(reportSrcFile);
+
+        HashMap<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("flightName", flight.getAirlineName() + " "+ flight.getCode());
+
+        JRBeanCollectionDataSource jrBeanList = new
+                JRBeanCollectionDataSource(passengerList);
+
+        JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, jrBeanList);
+        JRViewer viewer = new JRViewer(print);
+        viewer.setOpaque(true);
+        viewer.setVisible(true);
+        this.add(viewer);
+        this.setSize(700, 500);
+        this.setVisible(true);
+    }
+
+}
